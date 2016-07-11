a53ea8eff9a5ebbdf65d5aa5b774ff62b03e5e50
Carlo DiCelico <carlodicelico@me.com>
2016-07-10 22:56:54 -0400

    implement single photo view

--- learn-redux/src/single/Single.js
+++ learn-redux/src/single/Single.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Photo from '../photo/Photo';
+// import comments
 
 
 const Single = (props) => {
+  const i = props.posts.findIndex((post) =>
+    post.code === props.params.postId
+  );
+  const post = props.posts[i];
+
   return (
     <div className="single-photo">
-      <h3>Single photo</h3>
+      <Photo i={i} post={post} {...props} />
     </div>
   );
 };
