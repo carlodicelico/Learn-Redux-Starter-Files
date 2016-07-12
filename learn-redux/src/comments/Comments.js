88c10d3f27006e396ec12b83f5197800f2073a48
Carlo DiCelico <carlodicelico@me.com>
2016-07-12 14:27:07 -0400

    show comment author, etc; fall back to regular form for now

--- learn-redux/src/comments/Comments.js
+++ learn-redux/src/comments/Comments.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CommentForm from './CommentForm';
+//import CommentForm from './CommentForm';
 
 
 const Comment = (props) => {
@@ -15,6 +15,11 @@ const Comment = (props) => {
   )
 }
 
+/*
+
+Eventually, I want this to be a stateless functional component and use a
+redux-form form instead of a regular html form
+
 const Comments = (props) => {
   return (
     <div className='comments'>
@@ -25,5 +30,30 @@ const Comments = (props) => {
     </div>
   );
 };
+*/
+
+const Comments = React.createClass({
+  handleSubmit(e) {
+    e.preventDefault();
+    const { postId } = this.props.params,
+      author = this.refs.author.value,
+      comment = this.refs.comment.value;
+      this.props.addComment(postId, author, comment);
+  },
+  render() {
+    return (
+      <div className='comments'>
+        {this.props.postComments.map((comment, index) =>
+          <Comment {...this.props} key={index} index={index} comment={comment} />
+        )}
+        <form ref='commentForm' className='comment-form' onSubmit={this.handleSubmit}>
+          <input type='text' ref='author' placeholder='author' />
+          <input type='text' ref='comment' placeholder='comment' />
+          <input type='submit' hidden />
+        </form>
+      </div>
+    );
+  }
+});
 
 export default Comments;
