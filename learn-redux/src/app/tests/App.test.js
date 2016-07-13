f8dda6a3c557d78f672a87bfd4b6e84b0df750f1
Carlo DiCelico <carlodicelico@me.com>
2016-07-12 20:44:53 -0400

    stub out test files

--- learn-redux/src/app/tests/App.test.js
+++ learn-redux/src/app/tests/App.test.js
@@ -1,22 +1,6 @@
-import React from 'react';
 import test from 'ava';
-import { Provider } from 'react-redux';
-import { shallow } from 'enzyme';
-
-import App from '../App';
-import appStore from '../appStore';
 
 
-test.beforeEach(t => {
-  console.log('calling beforeEach callback');
-
-  t.context.wrapper = shallow(
-    <Provider store={appStore}>
-      <App />
-    </Provider>
-  );
-});
-
-test('renders correctly', t => {
-  t.true(t.context.wrapper.contains(<h1>AppStar</h1>));
+test('foo', t => {
+    t.pass();
 });
